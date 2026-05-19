7ef3797b222ac289f57080253e8f4c03dfd5313a
Sam <doansamquoc@gmail.com>
2026-05-20 05:09:40 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+<p align="center">
+    <img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNGFqbXphMjQ4dXNrNTdjMnI2NGZhcmx0eXRnNGhkaDRkdGk3bzY5cSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/ix8dIWbEovToc/giphy.gif"/>
+</p>
---
+++ src/main/java/chatapi/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package chatapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Configuration
+@EnableMethodSecurity
+@EnableWebSecurity
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(AbstractHttpConfigurer::disable);
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+}
