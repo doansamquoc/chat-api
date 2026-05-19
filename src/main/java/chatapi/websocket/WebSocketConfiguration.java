c9d22cbd06ed37ab4e7946c1a149bc85a5fd423d
Sam <doansamquoc@gmail.com>
2026-05-20 05:51:58 +0700

    feat: implement websocket configuration

---
+++ src/main/java/chatapi/websocket/WebSocketConfiguration.java
@@ -0,0 +1,26 @@
+package chatapi.websocket;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.jspecify.annotations.NonNull;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocketMessageBroker
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+    public void configureMessageBroker(@NonNull MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic",  "/queue");
+    }
+}
