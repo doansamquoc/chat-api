38633d8df9866bbbefb86aa41995890277adb80a
Sam <doansamquoc@gmail.com>
2026-05-29 13:29:17 +0700

    refactor: remove the dash in username generator

--- src/main/java/org/sam/chatapi/util/UserUtils.java
+++ src/main/java/org/sam/chatapi/util/UserUtils.java
@@ -11,39 +11,39 @@ import static org.sam.chatapi.constant.AuthConstant.AUTHORITY_PREFIX;
 
 public class UserUtils {
 
-	/**
-	 * Generate username by user's email
-	 *
-	 * @param email String
-	 * @return Unique username
-	 */
-	public static String generateUsername(String email) {
-		Random random = new Random();
-		int randomInt = random.nextInt(9999);
-		String name = email.split("@")[0];
-		return String.format("%s_%s", name, randomInt);
-	}
+    /**
+     * Generate username by user's email
+     *
+     * @param email String
+     * @return Unique username
+     */
+    public static String generateUsername(String email) {
+        Random random = new Random();
+        int randomInt = random.nextInt(9999);
+        String name = email.split("@")[0];
+        return String.format("%s%s", name, randomInt);
+    }
 
-	/**
-	 * Enrich user role
-	 *
-	 * @param role Role
-	 * @return GrantedAuthority
-	 */
-	public static GrantedAuthority toAuthority(String role) {
-		return new SimpleGrantedAuthority(AUTHORITY_PREFIX + role);
-	}
+    /**
+     * Enrich user role
+     *
+     * @param role Role
+     * @return GrantedAuthority
+     */
+    public static GrantedAuthority toAuthority(String role) {
+        return new SimpleGrantedAuthority(AUTHORITY_PREFIX + role);
+    }
 
-	/**
-	 * Only one role in authorities, just use findFirst to get role
-	 *
-	 * @param authorities GrantedAuthority Collection
-	 * @return Role
-	 */
-	public static Role toRole(Collection<? extends GrantedAuthority> authorities) {
-		String authority = authorities.stream().findFirst().map(GrantedAuthority::getAuthority).orElse(null);
-		if (authority == null) return null;
-		String roleName = authority.replace(AUTHORITY_PREFIX, "").trim();
-		return Role.valueOf(roleName);
-	}
+    /**
+     * Only one role in authorities, just use findFirst to get role
+     *
+     * @param authorities GrantedAuthority Collection
+     * @return Role
+     */
+    public static Role toRole(Collection<? extends GrantedAuthority> authorities) {
+        String authority = authorities.stream().findFirst().map(GrantedAuthority::getAuthority).orElse(null);
+        if (authority == null) return null;
+        String roleName = authority.replace(AUTHORITY_PREFIX, "").trim();
+        return Role.valueOf(roleName);
+    }
 }
